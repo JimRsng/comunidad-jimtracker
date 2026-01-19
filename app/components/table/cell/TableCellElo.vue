b6b1e27d7fbdb2721a18393e9ea23d2dcaeb9926
Yizack Rangel <yizackr@gmail.com>
2026-01-19 00:49:09 -0500

    chore: remove title attribute from tier image in TableCellElo

--- app/components/table/cell/TableCellElo.vue
+++ app/components/table/cell/TableCellElo.vue
@@ -12,7 +12,6 @@ const tablePopover = useTablePopover();
       :src="`/images/lol/${data.tier?.toLowerCase() || 'unranked'}.png`"
       class="w-10 h-10 md:w-10 md:h-10 max-w-fit"
       :alt="data.tier || 'UNRANKED'"
-      :title="data.tier || 'UNRANKED'"
       v-on="tablePopover.handlers(data.tier || 'UNRANKED')"
     >
     <span v-if="data.tier && (data.division || data.lp)"><span v-if="!['MASTER', 'GRANDMASTER', 'CHALLENGER'].includes(data.tier)">{{ data.division }} · </span>{{ data.lp }} LP</span>
