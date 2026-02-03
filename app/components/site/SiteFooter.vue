c88667a7639e99f241606b895bf3569e0125ed38
Yizack Rangel <yizackr@gmail.com>
2026-02-03 00:46:50 -0500

    feat: show build info in site footer

--- app/components/site/SiteFooter.vue
+++ app/components/site/SiteFooter.vue
@@ -1,7 +1,34 @@
+<script setup lang="ts">
+import { useTimeAgoIntl } from "@vueuse/core";
+
+const config = useRuntimeConfig();
+const buildInfo = config.public.buildInfo;
+
+const timeAgo = useTimeAgoIntl(buildInfo.time, {
+  locale: "es"
+});
+</script>
+
 <template>
   <UFooter class="border-t border-default">
     <template #left>
-      <p class="text-muted text-sm">{{ SITE.name }} © {{ new Date().getFullYear() }}</p>
+      <div class="flex flex-col lg:flex-row items-center gap-2">
+        <p class="text-muted text-sm">{{ SITE.name }} © {{ new Date().getFullYear() }}</p>
+        <UPopover mode="hover" :content="{ side: 'top' }" arrow>
+          <NuxtLink :to="`${SITE.github.repository}/commit/${buildInfo.commit}`" target="_blank">
+            <UBadge
+              color="neutral"
+              variant="subtle"
+              class="flex hover:text-highlighted hover:underline text-muted"
+            >
+              <span class="font-mono">Build {{ buildInfo.shortCommit }}</span>
+            </UBadge>
+          </NuxtLink>
+          <template #content>
+            <p>{{ timeAgo }}</p>
+          </template>
+        </UPopover>
+      </div>
     </template>
     <span class="text-sm">
       <NuxtLink :to="SITE.github.repository" target="_blank" class="dark:text-blue-300 light:text-blue-500 font-semibold hover:underline">Creado</NuxtLink>
@@ -16,7 +43,7 @@
     </span>
     <template #right>
       <NuxtLink :to="SITE.github.repository" target="_blank" class="hover:text-highlighted">
-        <Icon name="simple-icons:github" class="w-8 h-8" />
+        <Icon name="simple-icons:github" size="2rem" />
       </NuxtLink>
     </template>
   </UFooter>
